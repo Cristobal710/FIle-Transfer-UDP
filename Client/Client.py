ac34663d16256316bd3b5f5d4c51afd1a8d658f4
Cristobal <cristobal230502@gmail.com>
2025-09-13 17:25:15 -0300

    al introducir un nombre para el archivo y un path a dicho archivo, el cliente puede hacer UPLOAD de un archivo al servidor, con ACKs cada 25 bytes enviados, solo funciona cuando esta todo en orden, no se contemplan errores de ningun tipo, pero la transferencia del archivo se realiza de manera completa

---
+++ Client/Client.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Constants import *
+from Protocol.Archive import ArchiveSender
+
+if __name__ == "__main__":
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(TUPLA_DIR_CLIENT)
+    while True:
+        #caso de UPLOAD
+        name = input("Nombre del archivo: ")
+        path = input("Path: ")
+        arch = ArchiveSender(path) 
+        sock.sendto(name.encode(), TUPLA_DIR_ENVIO)
+        while True:
+            pkg = arch.next_pkg()
+
+            if (pkg == None):
+                break
+            else: 
+                sock.sendto(pkg, TUPLA_DIR_ENVIO)
+                pkg, addr = sock.recvfrom(1024)
+                print(pkg)
+                #while pkg == 'NAK':
+                    #REENVIAR
---
+++ Protocol/Archive.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Constants import *
+
+class ArchiveSender:
+    def __init__(self, path, ):
+        self.archivo = open(path, "rb")
+        self.last_byte_sent = 0
+        
+
+    def next_pkg(self):
+        msg = self.archivo.read(SIZE_PKG)
+        if not msg:
+            return None
+        
+        header = SIZE_PKG.to_bytes(4, 'big') #+ self.last_byte_sent.to_bytes(4, 'big')
+        pkg = header + msg
+        return pkg
+
+class ArchiveRecv:
+    def __init__(self, path, ):
+        self.archivo = open(path, "wb+")
+
+    def recv_pckg(self, msg):
+        #sz_msg = int.from_bytes(msg[0:4], 'big')
+        pkg = msg[4:]
+        self.archivo.write(pkg)
+        self.archivo.flush()
+
+
+        
