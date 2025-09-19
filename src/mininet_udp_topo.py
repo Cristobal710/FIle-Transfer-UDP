cc8625f101557e59596d0a4d29420a6c714ceed1
Cristobal <cristobal230502@gmail.com>
2025-09-19 15:18:55 -0300

    Agregado mininet, se corre con sudo python3 y path hasta mininet_udp_topo.py file, en ese archivo hay paths que son solo de mi computadora asique hay que cambiarlos para que tome el path correcto segun en que computadora se inicializa. Al correr el comando se levantan 2 terminales, una corre el servidor en un nodo h1 con IP 10.0.0.1 y la otra levanta el cliente en nodo h2 con IP 10.0.0.2, el caso de UPLOAD funciona correctamente, el caso de DOWNLOAD probablemente no porque hay que agregar una serie de chequeos de ACKs

---
+++ src/mininet_udp_topo.py
@@ -0,0 +1,38 @@
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.node import Controller, OVSKernelSwitch
+
+def start_network():
+    net = Mininet(controller=Controller, switch=OVSKernelSwitch)
+
+    # Add controller
+    net.addController('c0')
+
+    # Add hosts and switch
+    h1 = net.addHost('h1', ip='10.0.0.1', cwd='/home/cristobal/Escritorio/Redes/FIle-Transfer-UDP/src/server')
+    h2 = net.addHost('h2', ip='10.0.0.2', cwd='/home/cristobal/Escritorio/Redes/FIle-Transfer-UDP/src/client')
+    s1 = net.addSwitch('s1')
+
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+
+    net.start()
+
+    #Start tcpdump on server (h1)
+    tcpdump_h1 = h1.popen('tcpdump -i h1-eth0 udp -w /home/cristobal/Escritorio/wireshark_files/h1_capture.pcap')
+
+    # Start tcpdump on client (h2)
+    tcpdump_h2 = h2.popen('tcpdump -i h2-eth0 udp -w /home/cristobal/Escritorio/wireshark_files/h2_capture.pcap')
+
+    h1.cmd('xterm -hold -e "cd /home/cristobal/Escritorio/Redes/FIle-Transfer-UDP/src/server; python3 server.py; bash" &')
+    h2.cmd('xterm -hold -e "cd /home/cristobal/Escritorio/Redes/FIle-Transfer-UDP/src/client; python3 client.py; bash" &')
+
+    CLI(net)
+    h1.cmd('killall xterm')
+    h2.cmd('killall xterm')
+    tcpdump_h1.terminate()
+    tcpdump_h2.terminate()
+    net.stop()
+
+if __name__ == '__main__':
+    start_network()
