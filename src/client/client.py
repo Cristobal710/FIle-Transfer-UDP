2a4af7560e426bab60452403b7ff455b839decab
Lucas Aldonate <lucasaldonate@gmail.com>
2025-09-19 19:24:42 -0300

    fix: aplico linter y formateo codigo

--- src/client/client.py
+++ src/client/client.py
@@ -1,20 +1,20 @@
 import socket
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from constants import *
-from utils import *
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from constants import TUPLA_DIR_CLIENT
+from utils import download_file, upload_file
 
 if __name__ == "__main__":
-    
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(TUPLA_DIR_CLIENT)
     end = False
-    #while (not end):
+    # while (not end):
     type_conexion = input("Elegir Upload o Download (Escribir U o D para elegir): ")
-    if (type_conexion == 'D'):
-        download_file(sock,end)
-    elif (type_conexion == 'U'):
-        upload_file(sock, end)    
-    
+    if type_conexion == "D":
+        download_file(sock, end)
+    elif type_conexion == "U":
+        upload_file(sock, end)
+
     sock.close()
