f0b01addc1f69050134b0e186bc3ed017fc84018
Lucas Aldonate <lucasaldonate@gmail.com>
2025-09-19 18:59:27 -0300

    refactor: borro rutas hardcodeadas y modifico uso del ovs controller

--- src/mininet_udp_topo.py
+++ src/mininet_udp_topo.py
@@ -5,12 +5,12 @@ from mininet.node import Controller, OVSKernelSwitch
 def start_network():
     net = Mininet(controller=Controller, switch=OVSKernelSwitch)
 
-    # Add controller
-    net.addController('c0')
+    # Add controller with explicit executable
+    net.addController('c0', controller=Controller, command='ovs-testcontroller')
 
     # Add hosts and switch
-    h1 = net.addHost('h1', ip='10.0.0.1', cwd='/home/cristobal/Escritorio/Redes/FIle-Transfer-UDP/src/server')
-    h2 = net.addHost('h2', ip='10.0.0.2', cwd='/home/cristobal/Escritorio/Redes/FIle-Transfer-UDP/src/client')
+    h1 = net.addHost('h1', ip='10.0.0.1', cwd='src/server')
+    h2 = net.addHost('h2', ip='10.0.0.2', cwd='src/client')
     s1 = net.addSwitch('s1')
 
     net.addLink(h1, s1)
@@ -19,13 +19,13 @@ def start_network():
     net.start()
 
     #Start tcpdump on server (h1)
-    tcpdump_h1 = h1.popen('tcpdump -i h1-eth0 udp -w /home/cristobal/Escritorio/wireshark_files/h1_capture.pcap')
+    tcpdump_h1 = h1.popen('tcpdump -i h1-eth0 udp -w wireshark_files/h1_capture.pcap')
 
     # Start tcpdump on client (h2)
-    tcpdump_h2 = h2.popen('tcpdump -i h2-eth0 udp -w /home/cristobal/Escritorio/wireshark_files/h2_capture.pcap')
+    tcpdump_h2 = h2.popen('tcpdump -i h2-eth0 udp -w wireshark_files/h2_capture.pcap')
 
-    h1.cmd('xterm -hold -e "cd /home/cristobal/Escritorio/Redes/FIle-Transfer-UDP/src/server; python3 server.py; bash" &')
-    h2.cmd('xterm -hold -e "cd /home/cristobal/Escritorio/Redes/FIle-Transfer-UDP/src/client; python3 client.py; bash" &')
+    h1.cmd('xterm -hold -e "cd src/server; python3 server.py; bash" &')
+    h2.cmd('xterm -hold -e "cd src/client; python3 client.py; bash" &')
 
     CLI(net)
     h1.cmd('killall xterm')
