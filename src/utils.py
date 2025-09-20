2e9d20adf264a02ec59850b2d6e465fa8779e5d3
milithome <mthome@fi.uba.ar>
2025-09-20 20:56:17 -0300

    update: Todavia no corre bien el upload (queda en bucle infinito)

--- src/utils.py
+++ src/utils.py
@@ -41,8 +41,8 @@ def upload_file(sock: socket, end, protocolo=PROTOCOLO):
 
     if protocolo == STOP_AND_WAIT:
         upload_stop_and_wait(sock, arch, end)
-    #elif protocolo == GO_BACK_N:
-    #    upload_go_back_n(sock, arch, end, window_size=4)
+    elif protocolo == GO_BACK_N:
+        upload_go_back_n(sock, arch, end, window_size=4)
 
 def download_stop_and_wait(sock: socket, arch: ArchiveRecv, end):
     seq_expected = 0
@@ -83,7 +83,83 @@ def download_file(sock: socket, end, protocolo=PROTOCOLO):
 
     if protocolo == STOP_AND_WAIT:
         download_stop_and_wait(sock, arch, end)
-    #elif protocolo == GO_BACK_N:
-    #    download_go_back_n(sock, arch, end, window_size=4)
+    elif protocolo == GO_BACK_N:
+        download_go_back_n(sock, arch, end, window_size=4)
 
-    
+
+def upload_go_back_n(sock: socket, arch: ArchiveSender, end, window_size):
+    base = 0
+    next_seq_num = 0
+    window = {} #guardo los que estan espersndo a ser ackeados
+
+    sock.settimeout(None)   # arranca sin temporizador
+
+    while not end or base < next_seq_num:
+        
+        while next_seq_num < base + window_size and not end: # mando paquetes mientras haya espacio en la ventana
+            pkg = arch.next_pkg(next_seq_num)
+            if pkg is None:       # ya vio todo el archivo
+                pkg = END.encode()
+                end = True
+
+            sock.sendto(pkg, TUPLA_DIR_ENVIO)
+            print(f">>> Envié paquete {next_seq_num}")
+            window[next_seq_num] = pkg
+
+            if base == next_seq_num:# si es el primer paquete pendiente → arranco temporizador
+                sock.settimeout(ACK_TIMEOUT)
+
+            next_seq_num += 1
+
+        try: # espero que llegue el ACK
+            ack_msg, _ = sock.recvfrom(1024)
+            ack_num = int(ack_msg.decode().replace("ACK", ""))
+            print(f">>> Recibí ACK{ack_num}")
+
+            base = ack_num + 1 # la nueva base depende el numero de ACK q recibo
+
+            if base == next_seq_num: # ventana vacía → paro temporizador
+                sock.settimeout(None)
+            else:
+                sock.settimeout(ACK_TIMEOUT)
+
+        except socket.timeout:
+            print("Timeout, reenvío ventana...")
+            
+            for seq in range(base, next_seq_num): # retransmite todos los paquetes pendientes
+                sock.sendto(window[seq], TUPLA_DIR_ENVIO)
+                print(f">>> Reenvié paquete {seq}")
+            sock.settimeout(ACK_TIMEOUT)
+
+
+
+def download_go_back_n(sock: socket, arch: ArchiveRecv, end, window_size):
+    expected_seq_num = 0
+    work_done = False
+
+    while not work_done:
+        pkg, addr = sock.recvfrom(1024)
+
+        if pkg == END.encode():#ya se envio todo el archivo
+            print(">>> Cliente: transferencia finalizada.")
+            work_done = True
+           
+            sock.sendto(f"ACK{expected_seq_num}".encode(), addr) # mando ACK final
+            arch.archivo.close()
+            break
+
+        
+        seq_num, data_len, data = arch.recv_pckg(pkg) # siguen habiendo paquetes
+        print(f">>> Recibí paquete seq={seq_num}, esperado={expected_seq_num}, len={data_len}")
+
+        if seq_num == expected_seq_num:
+           
+            arch.archivo.write(data) # paquete correcto → lo escribo y confirmo
+            arch.archivo.flush()
+            sock.sendto(f"ACK{seq_num}".encode(), addr)
+            print(f">>> Envío ACK{seq_num}")
+            expected_seq_num += 1
+        else:
+            # paquete fuera de orden → descartar y repetir último ACK válido
+            sock.sendto(f"ACK{expected_seq_num - 1}".encode(), addr)
+            print(f">>> Paquete fuera de orden, reenvío ACK{expected_seq_num - 1}")
