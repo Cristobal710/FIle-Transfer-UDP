32ebbbf5f63a3209a1b8bb11e9ab52f8f7cabc60
Mateo Castaño <mateo.castano@vicentelopez.gov.ar>
2025-09-25 15:39:31 -0300

    refactor:modularizo handshake

--- src/protocol/protocol.py
+++ src/protocol/protocol.py
@@ -1,1 +1,8 @@
-# class Protocol:
+import socket
+from utils import stop_and_wait
+from constants import TUPLA_DIR_ENVIO
+
+def handshake(sock: socket, name: str, type: str):
+    stop_and_wait(sock, type.encode(), TUPLA_DIR_ENVIO) # send type of conexion to server
+    stop_and_wait(sock, name.encode(), TUPLA_DIR_ENVIO) # send file name to server
+  
